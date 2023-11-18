4c23c1a9bb5966efcfeb9011158c3994cc48eb08
Jeffrey Loaiza Quiceno <jeffreyloaiza92@hotmail.com>
2023-11-18 17:48:11 -0500

    Actualizacion estructura y busquedas

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from controller.FileLoad import FileLoad
-from controller.SearchFunctions import SearchFunctions
-
-#from Models.Node import Node
-#from Models.Matrix import Matrix
+from controllers.FileLoad import FileLoad
+from controllers.SearchFunctions import SearchFunctions
+from models.Node import Node
+from models.Matrix import Matrix
 
 
 def main():
@@ -12,6 +11,29 @@ def main():
     for i in range(len(matrizArchivo)):
         
         print(matrizArchivo[i])
+    matrix = Matrix(matrizArchivo)
+    inicio = Node(0,0)
+    objetivo = Node(2,5)
+    nodes = matrix.createAdjacencies(objetivo,"E")
+    funciones = SearchFunctions(nodes,matrix.matriz)
+    recorridoAnchura = funciones.RecorridoEnAnchura(inicio, objetivo)
+    recorridoProfundidad = funciones.recorrido_en_profundidad(inicio, objetivo)
+    recorridoCostoUniforme = funciones.recorrido_costo_uniforme(inicio, objetivo, "E")
+    recorridoBeamSearch = funciones.recorrido_beam_search(inicio, objetivo, "E")
+    print("Busqueda anchura")
+    for elemento in recorridoAnchura:
+        print("x: "+str(elemento.x)+ " y: "+str(elemento.y))
+    print("Busqueda profundidad")
+    for elemento in recorridoProfundidad:
+        print("x: "+str(elemento.x)+ " y: "+str(elemento.y))
+    print("Busqueda costo uniforme")
+    for elemento in recorridoCostoUniforme:
+        print("x: "+str(elemento.x)+ " y: "+str(elemento.y))
+    print("Busqueda beam search")
+    for elemento in recorridoBeamSearch:
+        print("x: "+str(elemento.x)+ " y: "+str(elemento.y))
+    for nodo in recorridoBeamSearch:
+        print(f"x: {nodo.x} y: {nodo.y} heuristica: {nodo.heuristica}")
     
 
 
